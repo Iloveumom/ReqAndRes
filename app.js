934295907842c15af9042eefcf0357401bda4b5e
Iloveumom <jtndrbhatia93@gmail.com>
2026-01-07 11:28:27 +0530

    some changes

--- app.js
+++ app.js
@@ -4,25 +4,27 @@ const http=require("http");
 const server=http.createServer((req,res)=>{
         console.log("server created");
         res.setHeader('Content-Type','text/html');
-        if(req.url==='/')
+        if(req.url==='/home')
         {
                 res.statusCode=200;  //ok
-                res.end("<h1>Home page</h1>");
+                res.end("<h1>Welcome home</h1>");
         }
-        else{
-            if(req.url==='/pizza')
+        else if(req.url==='/about')
+        {
+                res.statusCode=200;  //ok
+                res.end("<h1>'Welcome to About Us'</h1>");
+        }
+        else if(req.url==='/node')
             {
                 
                 res.statusCode=200;  //ok
-                res.end("<h1>This is your pizza</h1>");
+                res.end("<h1> 'Welcome to my Node Js project'</h1>");
             }
             else
-            {
-                
+                {
                 res.statusCode=404;  //not found
-                res.end("<h1>Page not found!</h1>");
+                res.end("<h1> page not found!!</h1>");
             }
-        }
 });
 
 const port =3000;
